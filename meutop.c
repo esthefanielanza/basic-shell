89ac75c1ad9c497e7314e65ac6be1512ddf7b314
gabrielcrds <gabriel.d.a.cardoso@gmail.com>
2018-04-06 17:04:13 -0300

    Começa a formatar tabela

--- meutop.c
+++ meutop.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
+
 int main(){
+  printf("PID    | User    | PROCNAME | Estado |\n");
+  printf("-------|---------|----------|--------|\n");
+
   DIR * dir = opendir("/proc/");
   struct dirent *ent;
   if(dir != NULL){
     // print all files within directory
-    while((ent = readdir(dir)) != NULL)
-      printf("%s\n", ent->d_name);
+    int pid, count = 0;
+    while((ent = readdir(dir)) != NULL && count < 20){
+      pid = atoi(ent->d_name);
+      if(pid){
+        printf("%7d\n", atoi(ent->d_name));
+        count++;
+      }
+    }
     closedir(dir);
   }
   else{
