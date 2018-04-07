931b5867e3d069441b59c152bfabdb3321d19633
gabrielcrds <gabriel.d.a.cardoso@gmail.com>
2018-04-07 17:24:43 -0300

    Adiciona funcionalidades do TOP

--- meutop.c
+++ meutop.c
@@ -3,20 +3,53 @@
 #include <dirent.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pwd.h>
+#include <unistd.h>
 
-int main(){
-  printf("PID    | User    | PROCNAME | Estado |\n");
-  printf("-------|---------|----------|--------|\n");
+#define clear() printf("\033[H\033[J")
+
+struct process{
+  char p_name[20], p_state[20], char_trash[500];
+  int p_uid;
+}
+
+void printTopProcess(int top){
+  printf("PID  | User     | PROCNAME      | Estado |\n");
+  printf("-----|----------|---------------|--------|\n");
 
   DIR * dir = opendir("/proc/");
   struct dirent *ent;
+  char file_name[50], line[500], p_name[20], p_state[20], char_trash[500];
+  int  p_uid;
   if(dir != NULL){
     // print all files within directory
     int pid, count = 0;
-    while((ent = readdir(dir)) != NULL && count < 20){
+    while((ent = readdir(dir)) != NULL && count < top){
       pid = atoi(ent->d_name);
       if(pid){
-        printf("%7d\n", atoi(ent->d_name));
+        printf("%5d|", atoi(ent->d_name));
+
+        int i = 0;
+        sprintf(file_name, "/proc/%s/status", ent->d_name);
+        FILE * file = fopen(file_name, "r");
+        while(fgets(line, sizeof(line), file)){
+          if(i == 0){
+            sscanf(line, "%s %s", char_trash, p_name);
+          } else if(i == 1){
+            sscanf(line, "%s %s", char_trash, p_state);
+          }
+          else if(i == 7){
+            sscanf(line, "%s %d", char_trash, &p_uid);
+            break;
+          }
+          i++;
+        }
+        struct passwd *pw = getpwuid(p_uid);
+        if(pw) printf("%10s|", pw->pw_name);
+        p_name[14] = '\0';
+        printf("%15s|", p_name); // name
+        printf("%8s|\n", p_state); // state
+        fclose(file);
         count++;
       }
     }
@@ -24,7 +57,21 @@ int main(){
   }
   else{
     printf("Error opening directory\n");
-    return 1;
+    exit(1);
   }
+}
+
+void top(){
+  while(1){
+    clear();
+    printTopProcess(30);
+    sleep(1);
+  }
+}
+
+int main(){
+
   return 0;
 }
+
+//getent passwd 1008 | cut -d: -f1
